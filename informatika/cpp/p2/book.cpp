780eb449d9dd784d6ff073bd4df3e0a6dcf07d97
Pavel <pavelbog@mail.ru>
2017-06-05 12:36:50 +0500

    p2-2

--- informatika/cpp/p2/book.cpp
+++ informatika/cpp/p2/book.cpp
@@ -1,15 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/* 
- * File:   book.cpp
- * Author: pavel
- * 
- * Created on 29 мая 2017 г., 23:58
- */
 #include <iostream>
 #include <string>
 #include "book.h"
