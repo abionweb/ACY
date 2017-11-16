014e51dd4a688f2e95afefa0d1ceac8d6c3bc2ae
abionweb <pavelbog@mail.ru>
2017-11-16 18:59:07 +0500

    2

---
+++ informatika/cpp/calculator/main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : main.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Calculator
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
