e2597e8b2c18e43ebc6c468a2fd73e7804ac8d9c
abionweb <pavelbog@mail.ru>
2018-01-18 00:53:04 +0500

    2

--- informatika/cpp/p7/src/main.cpp
+++ informatika/cpp/p7/src/main.cpp
@@ -5,14 +5,25 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+#include "vector.h"
 #include <iostream>
-#include "vector.h";
+
 using namespace std;
 
 int main() {
-	vector<int>a(5,0);
+	Vector <int> a(5,0);
+	cout << "Enter the elements of the container a:" << endl;
 	cin >> a;
-	cout << a <<endl;
+	cout << "a: " << a << endl;
+	cout << "a[2]: " << a[2] << endl;
+	cout << "The length of a = " << a() << endl << endl;
+
+	Vector <int> b(10,1);
+	cout << "b: " << b << endl << "b = a;" << endl;
+	b = a;
+	cout << "b: " << b << endl;
+	b = a + 10;
+	cout << "b = a + 10;" << endl << "b: " <<  b << endl;
+
 	return 0;
 }
--- informatika/cpp/p7/src/vector.h
+++ informatika/cpp/p7/src/vector.h
@@ -5,20 +5,97 @@
 using namespace std;
 
 template <class T>
-class vector {
+class Vector {
 public:
-	vector(int s, T k);
-	vector(const vector<T>&a);
-	~vector();
-	vector& operator=(const vector<T>&a);
+	Vector(int s, T k);
+	Vector(const Vector<T>&a);
+	~Vector();
+	Vector& operator=(const Vector<T>&a);
 	T& operator[](int index);
-	vector operator+(const T k);
+	Vector operator+(const T k);
+	Vector operator+(const Vector<T>&a);
 	int operator()();
-	friend ostream& operator << <>(ostream& out, const vector<T>&a);
-	friend istream& operator >> <>(istream& in, vector<T>&a);
+	template <class _T> friend istream & operator>> (istream & in, const Vector<_T>&);
+	template <class _T> friend ostream & operator<< (ostream & out, Vector<_T>&a);
 private:
 	int size;
 	T*data;
 };
+
+template <class T>
+Vector<T>::Vector(int s, T k) {
+	size = s;
+	data = new T[size];
+	for(int i=0; i<size; i++) data[i] = k;
+}
+
+template <class T>
+Vector<T>::Vector(const Vector&a) {
+	size = a.size;
+	data = new T[size];
+	for(int i=0; i<size; i++) data[i] = a.data[i];
+}
+
+template <class T>
+Vector<T>::~Vector() {
+	delete []data;
+	data = 0;
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator=(const Vector<T>&a) {
+	if(this==&a) return *this;
+	size = a.size;
+	if (data!=0) delete[]data;
+	data=new T[size];
+	for(int i=0; i<size; i++) data[i] = a.data[i];
+	return *this;
+}
+
+template <class T>
+T& Vector<T>::operator[](int index) {
+	if (index<size)
+		{ return data[index]; }
+	else {
+		cout << endl << "Error! Index>size";
+	}
+}
+
+template <class T>
+Vector<T> Vector<T>::operator+(const T k) {
+	Vector<T> temp(size, k);
+	for(int i=0; i<size; ++i) temp.data[i]=data[i]+k;
+	return temp;
+}
+
+template <class T>
+Vector<T> Vector<T>::operator+(const Vector<T>&a) {
+	int max, min;
+	Vector<T> temp(1);
+	if (size > a.size) {
+		min = a.size;
+		temp = *this;
+	} else {
+		min = size;
+		temp = a;
+	}
+	for (int i=0; i<min; ++i) temp.data[i] = data[i] + a.data[i];
+	return temp;
+}
+
+template <class T>
+int Vector<T>::operator ()() { return size; }
+
+template <class T>
+istream&operator >> (istream&in, const Vector<T>&a) {
+	for(int i=0; i<a.size; ++i) in >> a.data[i];
+	return in;
+}
+
+template <class T>
+ostream&operator << (ostream&out, Vector<T>&a) {
+	for(int i=0; i<a.size; ++i) out << a.data[i] << " ";
+	return out;
+}
 
 #endif /* VECTOR_H_ */
